e16dc397d0889fef2ebe869b67970ad36427f9df
ucanfindme18 <39616391+ucanfindme18@users.noreply.github.com>
2024-05-13 22:40:19 +0800

    Update grabber.py

--- grabber.py
+++ grabber.py
@@ -37,8 +37,6 @@ def grab(url):
 
 print('#EXTM3U')
 print('#EXT-X-INDEPENDENT-SEGMENTS')
-
-print(banner)
 #s = requests.Session()
 with open('youtube_channel_info.txt') as f:
     for line in f:
@@ -46,11 +44,6 @@ with open('youtube_channel_info.txt') as f:
         if not line or line.startswith('~~'):
             continue
         if not line.startswith('https:'):
-            line = line.split('|')
-            ch_name = line[0].strip()
-            grp_title = line[1].strip().title()
-            tvg_logo = line[2].strip()
-            tvg_id = line[3].strip()
             print(f'')
         else:
             grab(line)
